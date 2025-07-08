2c9b3ab07c19a8a6c12923ebfa790b56fc04423b
seoyeon-haru <sy5792@naver.com>
2025-07-08 12:44:11 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+class StorePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  
+}
