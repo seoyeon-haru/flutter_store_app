32a4d4f1b9fb1e9e3ad6787c476b002ce8a74e00
seoyeon-haru <sy5792@naver.com>
2025-07-08 23:53:37 +0900

    SizedBox 사용

--- lib/main.dart
+++ lib/main.dart
@@ -24,38 +24,41 @@ class StorePage extends StatelessWidget {
           children: [
             // Text
             // Image
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Text(
-                    'Woman',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+            SizedBox(
+              height: 70,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'Woman',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  Text(
-                    'Kids',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    Spacer(),
+                    Text(
+                      'Kids',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  Text(
-                    'Shoes',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    Spacer(),
+                    Text(
+                      'Shoes',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  Text(
-                    'Bag',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    Spacer(),
+                    Text(
+                      'Bag',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Expanded(
